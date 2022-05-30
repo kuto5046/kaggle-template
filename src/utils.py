48706dde787330f21be036dec217bdaa24b9a15c
Kyohei Uto <h27.kubb@gmail.com>
2022-05-30 13:22:14 +0000

    add reduce_mem_usage

--- src/utils.py
+++ src/utils.py
@@ -1,40 +1,43 @@
 # Gistから必要なスニペットを取ってくる
 # https://gist.github.com/kuto5046
 import pandas as pd
-from sklearn.model_selection import StratifiedKFold
-from sklearn.utils import _deprecate_positional_args
+import numpy as np 
 
-class ContinuousStratifiedFold(StratifiedKFold):
+
+def reduce_mem_usage(df):
+    """ iterate through all the columns of a dataframe and modify the data type
+        to reduce memory usage.        
     """
-    stratified-K-Fold Splits for continuous target.
-    """
+    start_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
 
-    @_deprecate_positional_args
-    def __init__(self, n_splits=5, *, q=10, shuffle=False, random_state=None):
-        """
-        Args:
-            n_splits:
-                number of splits
-            q:
-                number of quantiles.
-                例えば10に設定されると, `y` を値に応じて 10 個の集合に分割し, 各 fold では train / valid に集合ごとの
-                割合が同じように選択されます.
-            shuffle:
-                True の時系列をシャッフルしてから分割します
-            random_state:
-                シャッフル時のシード値
-        Notes:
-            y の水準数 (i.e. uniqueな数値の数) よりも大きい値を設定すると集合の分割に失敗し split を実行する際に
-            ValueError となることがあります. その場合小さい `q` を設定することを検討してください
-        """
-        super().__init__(n_splits=n_splits, shuffle=shuffle,
-                         random_state=random_state)
-        self.q = q
+    for col in df.columns:
+        col_type = df[col].dtype
 
-    def split(self, X, y, groups=None):
-        try:
-            y_cat = pd.qcut(y, q=self.q).values.codes
-        except ValueError as e:
-            raise ValueError('Fail to quantile cutting (using pandas.qcut). '
-                             'There are cases where this value fails when you make it too large') from e
-        return super(ContinuousStratifiedFold, self).split(X, y_cat, groups=groups)
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+        else:
+            df[col] = df[col].astype('category')
+
+    end_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+
+    return df
