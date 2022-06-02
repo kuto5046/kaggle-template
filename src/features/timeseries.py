b421e5f099f8829806bcd60a2d5cf80ffe6ff455
Kyohei Uto <h27.kubb@gmail.com>
2022-06-02 12:42:20 +0000

    add datetime feature

--- src/features/timeseries.py
+++ src/features/timeseries.py
@@ -1,5 +1,7 @@
 import pandas as pd 
+import numpy as np 
 from typing import List 
+from datetime import timedelta
 
 def get_lag_features(df:pd.DataFrame, col: str, lag: int, group_cols:List[str]=None):
     """lag特徴量を作成する関数
@@ -39,3 +41,58 @@ def get_moving_average_features(df:pd.DataFrame, col, lag:int, window_size: int,
         df[f'{col}_rolling_min_{lag}_{window_size}_by_{group_str}']  = df.groupby(group_cols)[col].transform(lambda x: x.shift(lag).rolling(window_size).min())
     
     return df 
+
+
+def create_datetime_feature(
+    df: pd.DataFrame, 
+    col: str, 
+    prefix: str = None, 
+    change_utc2asia: bool = False,
+    attrs: list = ['year', 'quarter', 'month', 'week', 'day', 'dayofweek', 'hour', 'minute']) -> pd.DataFrame:
+    """日時特徴量の生成処理
+    Args:
+        df (pd.DataFrame): 日時特徴量を含むDF
+        col (str)): 日時特徴量のカラム名
+        prefix (str): 新しく生成するカラム名に付与するprefix
+        attrs (list of str): 生成する日付特徴量. Defaults to ['year', 'quarter', 'month', 'week', 'day', 'dayofweek', 'hour', 'minute']
+                             cf. https://qiita.com/Takemura-T/items/79b16313e45576bb6492
+    Returns:
+        pd.DataFrame: 日時特徴量を付与したDF
+
+    usage:
+    df = create_datatime_feature(df, col='timestamp', attrs=['year', 'month', 'week', 'day', 'dayofweek', 'hour', 'minute'])
+    """
+    if prefix is None:
+        prefix = col
+
+    # utc -> asia/tokyo
+    if change_utc2asia:
+        df.loc[:, col] = pd.to_datetime(df[col]) + timedelta(hours=9)
+    else:
+        df.loc[:, col] = pd.to_datetime(df[col])
+
+    for attr in attrs:
+        dtype = np.int16 if attr == 'year' else np.int8
+        df[prefix + '_' + attr] = getattr(df[col].dt, attr).astype(dtype)
+
+    # 土日フラグ
+    if 'dayofweek' in attrs:
+        df[prefix + '_is_weekend'] = df[prefix + '_dayofweek'].isin([5, 6]).astype(np.int8)
+
+    # 時間帯情報
+    if 'hour' in attrs:
+        df[prefix + '_hour_zone'] = pd.cut(df[prefix + '_' + 'hour'].values, bins=[-np.inf, 6, 12, 18, np.inf]).codes
+
+    # 日付の周期性を算出
+    def sin_cos_encode(df, col):
+        df[col + '_cos'] = np.cos(2 * np.pi * df[col] / df[col].max())
+        df[col + '_sin'] = np.sin(2 * np.pi * df[col] / df[col].max())
+        return df
+
+    for col in [prefix + '_' + 'quarter', prefix + '_' + 'month', prefix + '_' + 'day', prefix + '_' + 'dayofweek',
+                prefix + '_' + 'hour', prefix + '_' + 'minute', prefix + '_' + 'hour_zone']:
+        
+        if col in df.columns.tolist():
+            df = sin_cos_encode(df, col)
+
+    return df
