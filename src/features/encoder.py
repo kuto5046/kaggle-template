6c640c21272600b4f003e64ebca07a00b775f283
Kyohei Uto <h27.kubb@gmail.com>
2022-10-03 13:47:12 +0000

    encoder系を追加

--- src/features/encoder.py
+++ src/features/encoder.py
@@ -40,49 +40,52 @@ def pp_for_categorical_encoding(train: pd.DataFrame, test: pd.DataFrame, cols:Li
             print(f"Error at {col} columns")
     return train, test
 
-
+# encoder系はtaret encodingを除いて全て新しく作成されたcolumnのみを返す
 def count_encoder(train: pd.DataFrame, test: pd.DataFrame, cols: list) -> pd.DataFrame:
     """
     usage:
-    train, test = count_encoder(train, test, cols=['cat1', 'cat2'])
+    ce_train, ce_test = count_encoder(train, test, cols=['cat1', 'cat2'])
     """
+    prefix = 'count_enc'
     for col in cols:
         encoder = train[col].value_counts()
-        train[f'{col}_count_enc'] = train[col].map(encoder)
-        test[f'{col}_count_enc'] = test[col].map(encoder)
-    return train, test
+        train[f'{prefix}_{col}'] = train[col].map(encoder)
+        test[f'{prefix}_{col}'] = test[col].map(encoder)
+    _train = train.filter(like=prefix).astype('category')
+    _test = test.filter(like=prefix).astype('category')
+    return _train, _test
 
 
 def label_encoder(train: pd.DataFrame, test: pd.DataFrame, cols: list) -> pd.DataFrame:
     """
     ordinal_encoderと同じ挙動のはず
     usage:
-    train, test = label_encoder(train, test, cols=['cat1', 'cat2'])
+    oe_train, oe_test = label_encoder(train, test, cols=['cat1', 'cat2'])
     """
+    prefix = 'label_enc'
     for col in cols:
         encoder = LabelEncoder()
         _whole = pd.concat([train, test], axis=0).reset_index(drop=True)
         encoder.fit(_whole[col])
-        train[f'{col}_label_enc'] = encoder.transform(train[col])
-        test[f'{col}_label_enc'] = encoder.transform(test[col])
-    return train, test
+        train[f'{prefix}_{col}'] = encoder.transform(train[col])
+        test[f'{prefix}_{col}'] = encoder.transform(test[col])
+    _train = train.filter(like=prefix).astype('category')
+    _test = test.filter(like=prefix).astype('category')
+    return _train, _test
 
 
 def ordinal_encoder(train: pd.DataFrame, test: pd.DataFrame, cols: list) -> pd.DataFrame:
     """
     label_encoderと同じ挙動のはず
     usage:
-    train, test = ordinal_encoder(train, test, cols=['cat1', 'cat2'])
+    le_train, le_test = ordinal_encoder(train, test, cols=['cat1', 'cat2'])
     """
     encoder = OrdinalEncoder()
     _whole = pd.concat([train, test], axis=0).reset_index(drop=True)
     encoder.fit(_whole[cols])
-    _train = pd.DataFrame(encoder.transform(train[cols]), columns=cols).add_suffix('_ordinal_enc')
-    _test = pd.DataFrame(encoder.transform(test[cols]), columns=cols).add_suffix('_ordinal_enc')
-    train = pd.concat([train, _train], axis=1)
-    test = pd.concat([test, _test], axis=1)
-    return train, test
-
+    _train = pd.DataFrame(encoder.transform(train[cols]), columns=cols).add_prefix('ordinal_enc_').astype('category')
+    _test = pd.DataFrame(encoder.transform(test[cols]), columns=cols).add_prefix('ordinal_enc_').astype('category')
+    return _train, _test
 
 
 def target_encoder(train: pd.DataFrame, test: pd.DataFrame, col: str, target_col: str):
