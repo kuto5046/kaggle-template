9b4b885c089571baddc962cf86903498b23f371d
Kyohei Uto <h27.kubb@gmail.com>
2023-11-25 10:47:44 +0900

    Add new files and update dependencies

--- src/features/base.py
+++ src/features/base.py
@@ -1,35 +1,62 @@
-import pandas as pd
-from abc import ABCMeta, abstractmethod
-import time 
-from pathlib import Path
-from contextlib import contextmanager
 import inspect
+import time
+from abc import ABCMeta
+from abc import abstractmethod
+from contextlib import contextmanager
+from pathlib import Path
 from typing import Optional
-from src.utils import reduce_mem_usage
+
+import pandas as pd
+
+from src.utils.common import reduce_mem_usage
 
 
-def get_categorical_col(df:pd.DataFrame, skip_cols: list=[]):
+def get_categorical_col(df: pd.DataFrame, skip_cols: list = []):
     """カテゴリ型のカラム名を取得"""
     category_cols = []
-    numerics = ['int8', 'int16', 'int32', 'int64', 'uint8', 'uint16', 'uint32', 'uint64', 'float16', 'float32', 'float64']
+    numerics = [
+        "int8",
+        "int16",
+        "int32",
+        "int64",
+        "uint8",
+        "uint16",
+        "uint32",
+        "uint64",
+        "float16",
+        "float32",
+        "float64",
+    ]
     for col in df.columns:
         if col in skip_cols:
-            continue 
+            continue
         col_type = df[col].dtypes
         if col_type not in numerics:
             category_cols.append(col)
     return category_cols
 
 
-def get_numerical_col(df:pd.DataFrame, skip_cols: list=[]):
+def get_numerical_col(df: pd.DataFrame, skip_cols: list = []):
     """数値型のカラム名を取得
     skip_colsにはtargetなど特徴量に追加しないものを選択
     """
     num_cols = []
-    numerics = ['int8', 'int16', 'int32', 'int64', 'uint8', 'uint16', 'uint32', 'uint64', 'float16', 'float32', 'float64']
+    numerics = [
+        "int8",
+        "int16",
+        "int32",
+        "int64",
+        "uint8",
+        "uint16",
+        "uint32",
+        "uint64",
+        "float16",
+        "float32",
+        "float64",
+    ]
     for col in df.columns:
         if col in skip_cols:
-            continue 
+            continue
         col_type = df[col].dtypes
         if col_type in numerics:
             num_cols.append(col)
@@ -39,56 +66,60 @@ def get_numerical_col(df:pd.DataFrame, skip_cols: list=[]):
 @contextmanager
 def timer(name):
     t0 = time.time()
-    print(f'[{name}] start')
+    print(f"[{name}] start")
     yield
-    print(f'[{name}] done in {time.time() - t0:.0f} s')
-
+    print(f"[{name}] done in {time.time() - t0:.0f} s")
 
 
 class Feature(metaclass=ABCMeta):
-    prefix = ''
-    suffix = ''
-    dir = '/home/user/work/features/'
+    prefix = ""
+    suffix = ""
+    dir = "/home/user/work/features/"
     fold_list = [0, 1, 2, 3, 4]  # 必要に応じて外から上書き
     """
     dirとfold listは以下のように外から上書きする
     Feature.dir = c.feature_dir
     Feature.fold_list = fold_list  # 特徴量を作成するfoldを指定
     """
-    
+
     def __init__(self):
         self.reset()
 
     def run(self):
-        prefix = self.prefix + '_' if self.prefix else ''
-        suffix = '_' + self.suffix if self.suffix else ''
+        prefix = self.prefix + "_" if self.prefix else ""
+        suffix = "_" + self.suffix if self.suffix else ""
         with timer(self.name):
             # train
             # こちらは検証時foldごとに作成する特徴量
             # 保存するファイル名にfoldが含まれている
             for fold in self.fold_list:
                 self.create_features(fold=fold)
-                
+
                 if self.train.shape[0] > 0:
                     self.train.columns = prefix + self.train.columns + suffix
-                    train_path_fold = self.train_path.with_name(f"{self.train_path.stem}_fold{fold}")
+                    train_path_fold = self.train_path.with_name(
+                        f"{self.train_path.stem}_fold{fold}"
+                    )
                     self.train = reduce_mem_usage(self.train)
                     self.train.to_pickle(f"{str(train_path_fold)}.pickle")
 
                 if self.valid.shape[0] > 0:
-                    self.valid.columns = prefix + self.valid.columns + suffix  
-                    valid_path_fold = self.valid_path.with_name(f"{self.valid_path.stem}_fold{fold}")
+                    self.valid.columns = prefix + self.valid.columns + suffix
+                    valid_path_fold = self.valid_path.with_name(
+                        f"{self.valid_path.stem}_fold{fold}"
+                    )
                     self.valid = reduce_mem_usage(self.valid)
                     self.valid.to_pickle(f"{str(valid_path_fold)}.pickle")
 
                 # 基本このphaseではself.testは作成しないが
-                # embeddingの次元を揃える必要がある場合はfoldごとにtestの特徴量を作成する必要があるためself.testを作成する
+                # embeddingの次元を揃える必要がある場合は
+                # foldごとにtestの特徴量を作成する必要があるためself.testを作成する
                 if self.test.shape[0] > 0:
-                    self.test.columns = prefix + self.test.columns + suffix  
+                    self.test.columns = prefix + self.test.columns + suffix
                     test_path_fold = self.test_path.with_name(f"{self.test_path.stem}_fold{fold}")
                     self.test = reduce_mem_usage(self.test)
                     self.test.to_pickle(f"{str(test_path_fold)}.pickle")
-                
+
                 self.reset()
 
             # sub用(foldを切らずtrain全体で学習する場合にtrainとtestの特徴量を作成する)
@@ -109,13 +140,13 @@ class Feature(metaclass=ABCMeta):
         self.train = pd.DataFrame()
         self.valid = pd.DataFrame()
         self.test = pd.DataFrame()
-        self.train_path = Path(self.dir) / f'{self.name}_train'
-        self.valid_path = Path(self.dir) / f'{self.name}_valid'
-        self.test_path = Path(self.dir) / f'{self.name}_test'
+        self.train_path = Path(self.dir) / f"{self.name}_train"
+        self.valid_path = Path(self.dir) / f"{self.name}_valid"
+        self.test_path = Path(self.dir) / f"{self.name}_test"
 
     @abstractmethod
-    def create_features(self, fold:Optional[int]=None):
-        """ 
+    def create_features(self, fold: Optional[int] = None):
+        """
         if fold is not None:
             # for cv
             _train, _valid = split_train_valid(train, fold)
@@ -127,7 +158,7 @@ class Feature(metaclass=ABCMeta):
             self.test = test[col].to_numpy()
         """
         raise NotImplementedError
-    
+
 
 def get_features(namespace):
     for k, v in namespace.items():
@@ -138,8 +169,8 @@ def get_features(namespace):
 def generate_features(namespace, overwrite: bool = False):
     for f in get_features(namespace):
         # foldごとのチェックが面倒なので一番最後に作成されるtestが存在するかで判定する
-        if f.test_path.with_suffix(f".pickle").exists() and not overwrite:
-            print(f.name, 'was skipped')
+        if f.test_path.with_suffix(".pickle").exists() and not overwrite:
+            print(f.name, "was skipped")
         else:
             f.run()
 
@@ -153,33 +184,37 @@ def remove_features(feature_dir: Path):
             filepath.unlink()
 
 
-def load_datasets(feats: list[str], input_dir: Path = Path('/home/user/work/features/'), phase: str='train', fold: Optional[int]=None) -> pd.DataFrame:
-    """ 
+def load_datasets(
+    feats: list[str],
+    input_dir: Path = Path("/home/user/work/features/"),
+    phase: str = "train",
+    fold: Optional[int] = None,
+) -> pd.DataFrame:
+    """
     testをfoldで区切る必要があるケースがややこしい
     foldを指定してtestを読む場合まずfoldごとのデータがある前提で読みにいく。ない場合はfoldなしのデータを読む
     """
-    assert phase in ['train', 'valid', 'test']
-    if phase == 'train':
+    assert phase in ["train", "valid", "test"]
+    if phase == "train":
         if fold is None:
             # foldを分割していないtrain全体の特徴量
-            dfs = [pd.read_pickle(input_dir / f'{f}_train.pickle') for f in feats]
+            dfs = [pd.read_pickle(input_dir / f"{f}_train.pickle") for f in feats]
         else:
-            dfs = [pd.read_pickle(input_dir / f'{f}_train_fold{fold}.pickle') for f in feats]
-    elif phase == 'valid':
-        dfs = [pd.read_pickle(input_dir / f'{f}_valid_fold{fold}.pickle') for f in feats]
+            dfs = [pd.read_pickle(input_dir / f"{f}_train_fold{fold}.pickle") for f in feats]
+    elif phase == "valid":
+        dfs = [pd.read_pickle(input_dir / f"{f}_valid_fold{fold}.pickle") for f in feats]
     else:
-        
         if fold is None:
             # foldに関係ないtestの特徴量
-            dfs = [pd.read_pickle(input_dir / f'{f}_test_fold{fold}.pickle') for f in feats]
+            dfs = [pd.read_pickle(input_dir / f"{f}_test_fold{fold}.pickle") for f in feats]
         else:
             # foldごとに作成しているtestの特徴量
             dfs = []
             for f in feats:
-                if (input_dir / f'{f}_test_fold{fold}.pickle').exists():
-                    dfs.append(pd.read_pickle(input_dir / f'{f}_test_fold{fold}.pickle'))
+                if (input_dir / f"{f}_test_fold{fold}.pickle").exists():
+                    dfs.append(pd.read_pickle(input_dir / f"{f}_test_fold{fold}.pickle"))
                 else:
-                    dfs.append(pd.read_pickle(input_dir / f'{f}_test.pickle'))
-    
+                    dfs.append(pd.read_pickle(input_dir / f"{f}_test.pickle"))
+
     X = pd.concat(dfs, axis=1)
     return X
---
+++ src/utils/visualize.py
@@ -0,0 +1,251 @@
+from pathlib import Path
+from typing import List
+
+import catboost
+import lightgbm
+import lightgbm as lgb
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import sweetviz as sv
+import xgboost
+from IPython.display import display_html
+from matplotlib_venn import venn2
+
+
+# 欠損値の確認
+def missing_values(data):
+    total = data.isnull().sum()
+    percent = data.isnull().sum() / data.isnull().count() * 100
+    tt = pd.concat([total, percent], axis=1, keys=["Total", "Percent"])
+    types = []
+    for col in data.columns:
+        dtype = str(data[col].dtype)
+        types.append(dtype)
+    tt["Types"] = types
+    return np.transpose(tt)
+
+
+# 頻出値の確認
+def most_frequent_values(data):
+    total = data.count()
+    tt = pd.DataFrame(total)
+    tt.columns = ["Total"]
+    items = []
+    vals = []
+    for col in data.columns:
+        itm = data[col].value_counts().index[0]
+        val = data[col].value_counts().values[0]
+        items.append(itm)
+        vals.append(val)
+    tt["Most frequent item"] = items
+    tt["Frequence"] = vals
+    tt["Percent from total"] = np.round(vals / total * 100, 3)
+    return np.transpose(tt)
+
+
+# ユニーク値の確認
+def unique_values(data):
+    total = data.count()
+    tt = pd.DataFrame(total)
+    tt.columns = ["Total"]
+    uniques = []
+    for col in data.columns:
+        unique = data[col].nunique()
+        uniques.append(unique)
+    tt["Uniques"] = uniques
+    tt["Percent"] = np.round(uniques / total * 100, 3)
+    return np.transpose(tt)
+
+
+# これを呼ぶ
+def show_all(df, n=5):
+    print(f"Data shape :{df.shape}")
+    df_describe = (
+        df.describe()
+        .style.set_table_attributes("style='display:inline'")
+        .set_caption("Describe Data Num")
+    )
+    df_describe_o = (
+        df.describe(include=[object])
+        .style.set_table_attributes("style='display:inline'")
+        .set_caption("Describe Data Object")
+    )
+    df_head = (
+        df.head(n).style.set_table_attributes("style='display:inline'").set_caption("Head Data")
+    )
+    df_tail = (
+        df.tail(n).style.set_table_attributes("style='display:inline'").set_caption("Tail Data")
+    )
+    df_missing = (
+        missing_values(df)
+        .style.set_table_attributes("style='display:inline'")
+        .set_caption("Missing Value")
+    )
+    df_frequent = (
+        most_frequent_values(df)
+        .style.set_table_attributes("style='display:inline'")
+        .set_caption("Frequent Value")
+    )
+    df_unique = (
+        unique_values(df)
+        .style.set_table_attributes("style='display:inline'")
+        .set_caption("Unique Value")
+    )
+
+    display_html(df_describe._repr_html_(), raw=True)
+    display_html(df_describe_o._repr_html_(), raw=True)
+    display_html(df_head._repr_html_(), raw=True)
+    display_html(df_tail._repr_html_(), raw=True)
+    display_html(df_missing._repr_html_(), raw=True)
+    display_html(df_frequent._repr_html_(), raw=True)
+    display_html(df_unique._repr_html_(), raw=True)
+    return None
+
+
+def pd_profiling(df: pd.DataFrame, output_dir: Path = Path("./")):
+    """
+    usage:
+    pd_profiling(whole)
+    """
+    import ydata_profiling
+
+    profile_report = ydata_profiling.ProfileReport(df)
+    profile_report.to_file(output_dir / "report.html")
+
+
+def sweetviz_report(
+    train: pd.DataFrame,
+    test: pd.DataFrame,
+    target_col: str = None,
+    skip_cols: List[str] = [],
+    output_dir: str = "./",
+):
+    """
+    usage:
+    sweetviz_report(train, test, target_col='target')
+    """
+
+    feature_config = sv.FeatureConfig(skip=skip_cols)
+
+    my_report = sv.compare(
+        [train, "Training Data"], [test, "Test Data"], target_col, feature_config
+    )
+    my_report.show_html(output_dir + "SWEETVIZ_REPORT.html")
+
+
+def get_uniques(input_df: pd.DataFrame, column):
+    s = input_df[column]
+    return set(s.dropna().unique())
+
+
+def plot_intersection(
+    left: pd.DataFrame,
+    right: pd.DataFrame,
+    target_column: str,
+    ax: plt.Axes = None,
+    set_labels: List[str] = None,
+):
+    venn2(
+        subsets=(get_uniques(left, target_column), get_uniques(right, target_column)),
+        set_labels=set_labels or ("Train", "Test"),
+        ax=ax,
+    )
+    ax.set_title(target_column)
+
+
+def plot_venn(train: pd.DataFrame, test: pd.DataFrame, output_dir: str = "./"):
+    """
+    usage:
+    plot_venn(train, test)
+    """
+    target_columns = test.columns.tolist()
+    n_cols = 5
+    n_rows = -(-len(target_columns) // n_cols)
+
+    fig, axes = plt.subplots(figsize=(4 * n_cols, 3 * n_rows), ncols=n_cols, nrows=n_rows)
+    for c, ax in zip(target_columns, np.ravel(axes)):
+        plot_intersection(train, test, target_column=c, ax=ax)
+    plt.savefig(output_dir + "venn.png")
+
+
+def plot_importance(
+    models: List[lgb.Booster]
+    | List[xgboost.core.Booster]
+    | List[catboost.core.CatBoostRegressor]
+    | List[catboost.core.CatBoostClassifier],
+    output_dir: Path = Path("./"),
+    importance_type="gain",
+    top_n: int = 50,
+):
+    """model 配列の feature importance を plot する
+    CVごとのブレを boxen plot として表現します.
+    args:
+        models:
+            List of lightGBM models
+        feature_cols:
+            学習時に使った特徴量リスト
+    usage:
+    ```
+    fig, ax = visualize_importance(models, train_feat_df)
+    ```
+    """
+    feature_importance_df = pd.DataFrame()
+    for i, model in enumerate(models):
+        _df = pd.DataFrame()
+
+        if isinstance(model, lightgbm.Booster):
+            _df["feature_importance"] = model.feature_importance(importance_type=importance_type)
+            _df["column"] = model.feature_name()
+        elif isinstance(model, xgboost.core.Booster):
+            feature_importance = model.get_score(importance_type=importance_type)
+            _df["feature_importance"] = feature_importance.values()
+            _df["column"] = feature_importance.keys()
+        elif isinstance(model, catboost.core.CatBoostRegressor) or isinstance(
+            model, catboost.core.CatBoostClassifier
+        ):
+            _df["feature_importance"] = model.get_feature_importance()
+            _df["column"] = model.feature_names_
+        else:
+            raise NotImplementedError
+        _df["fold"] = i + 1
+        feature_importance_df = pd.concat([feature_importance_df, _df], axis=0, ignore_index=True)
+
+    order = (
+        feature_importance_df.groupby("column")
+        .sum()[["feature_importance"]]
+        .sort_values("feature_importance", ascending=False)
+        .index[:top_n]
+    )
+
+    fig, ax = plt.subplots(figsize=(8, max(6, len(order) * 0.25)))
+    sns.boxenplot(
+        data=feature_importance_df,
+        x="feature_importance",
+        y="column",
+        order=order,
+        ax=ax,
+        palette="viridis",
+        orient="h",
+    )
+    ax.tick_params(axis="x", rotation=90)
+    ax.set_title("Importance")
+    ax.grid()
+    fig.tight_layout()
+    plt.savefig(output_dir / "importance.png")
+    return fig, ax
+
+
+def plot_shap(model, df, feature_cols, plot_type=None):
+    import shap
+
+    shap.initjs()
+    explainer = shap.TreeExplainer(model, data=df[feature_cols], check_additivity=False)
+    shap_values = explainer.shap_values(df[feature_cols])
+    shap.summary_plot(
+        shap_values=shap_values,
+        features=df[feature_cols],
+        feature_name=feature_cols,
+        plot_type=plot_type,
+    )
